94f0b711d792e5a35e587658f4640cfb8b9089ed
Sevilay01 <sevilaycelik829@gmail.com>
2024-10-12 12:48:06 +0300

    6th Problem

---
+++ ProjectEuler_6.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+int findsumofsquares(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        
+        sum += i * i;
+       
+    }
+    return sum;
+    
+}
+int findthesquareofthesum(int n) {
+    int sum2 = 0;
+    for (int i = 1; i <=n; i++) {
+        
+        sum2 += i;
+        
+    }
+    return sum2 * sum2;
+}
+int main()
+{
+    int result = findthesquareofthesum(100)-findsumofsquares(100);
+    cout << result;
+}
